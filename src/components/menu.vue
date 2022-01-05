95092f7741566f368b81f3eb2b870eb47e2e5246
dannyvg <dannyvangasteren14@gmail.com>
2022-01-05 16:49:35 +0100

    update

---
+++ src/components/menu.vue
@@ -0,0 +1,38 @@
+<template>
+ <ul id="example-1">
+  <li v-for="item in items" :key="item.message">
+    {{ item.message }}
+  </li>
+</ul>
+</template>
+
+<script>
+
+var example1 = new Vue({
+  el: '#example-1',
+  data: {
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
